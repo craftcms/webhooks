d38709eb1d4ac7dfb169865bbcc33f938255be89
Brandon Kelly <brandon@pixelandtonic.com>
2019-08-17 05:49:33 -0700

    Stop validating webhook URLs as URLs

--- src/Webhook.php
+++ src/Webhook.php
@@ -119,7 +119,6 @@ class Webhook extends Model
             [['groupId'], 'number'],
             [['enabled'], 'boolean'],
             [['method'], 'in', 'range' => ['get', 'post']],
-            [['url'], 'url'],
             [
                 ['class'],
                 function(string $attribute, array $params = null, Validator $validator) {
